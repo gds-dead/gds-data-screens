ef404e60d079b36f8dbe35a3663e34b8fe5cc77a
Mat Johnson <mat@demotive.com>
2014-01-09 13:31:20 +0000

    Setting up

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist : {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'public/css/main.css': 'assets/scss/main.scss'
+                }
+            } 
+        },
+        
+        concat: {
+            dist: {
+                src: [
+                    'assets/js/vendor/*.js',
+                    'assets/js/main.js'
+                ],
+                dest: 'public/js/app.js',
+            }
+        },
+    
+        uglify: {
+            build: {
+                src: 'public/js/app.js',
+                dest: 'public/js/app.min.js'
+            }
+        },
+    
+        watch: {
+            scripts: {
+                files: ['assets/js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['assets/scss/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            }
+        }
+    
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('default', ['watch']);
+
+};
