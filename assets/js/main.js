b52fbb402d01496e28672c03a5e44441d83fd57b
Mat Johnson <mat@demotive.com>
2014-01-09 14:09:41 +0000

    Initial slide motion (horizontal)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,48 @@
-var test = "TESTING!YES!";
+// very cheap and cheerful
+var monthsMap = {
+	"01" : "January",
+	"02" : "February",
+	"03" : "March",
+	"04" : "April",
+	"05" : "May",
+	"06" : "June",
+	"07" : "July",
+	"08" : "August",
+	"09" : "September",
+	"10" : "October",
+	"11" : "November",
+	"12" : "December"
+};
+
+// add commas to long numbers
+var addCommas = function(n) {
+	n += '';
+	x = n.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+
+// slide functionality
+var slides = document.getElementsByClassName('item');
+
+var cycleSlides = function() {
+	for (var i=0; i<slides.length; i++) {
+		slides[i].classList.remove('prev');
+	}
+	var current = document.getElementsByClassName('now')[0];
+	var next = current.nextElementSibling;
+	if (!next) {
+		next = slides[0];
+	}
+	current.classList.add('prev');
+	current.classList.remove('now');
+	next.classList.add('now');
+};
+
+var sliderTimer = window.setInterval(cycleSlides, 10000);
+
